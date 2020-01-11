21aad1e0d6c58cd0beae78d84a139b0b9a6cc6c0
Chamika Ravinda <chamikaravinda@gmail.com>
2020-01-11 22:47:23 +0530

    create block class

---
+++ block.js
@@ -0,0 +1,19 @@
+class Block {
+    constructor(timestamp,lastHash,hash,data){
+        this.timestamp = timestamp;
+        this.lastHash = lastHash;
+        this.hash = hash;
+        this.data = data;
+    }
+
+    toString(){
+        return `Block - 
+            Timestamp : ${this.timestamp}
+            Last Hast : ${this.lastHash.substring(0,10)}
+            Hash : ${this.hash}
+            Data : ${this.data}';
+        `
+    }
+}
+
+module.exports = Block;
