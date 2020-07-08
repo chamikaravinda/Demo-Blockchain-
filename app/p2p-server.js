51b8af57f4f91d5d1118a65e20277d6a841ee6e0
Chamika Ravinda <chamikaravinda@gmail.com>
2020-07-08 13:26:41 +0530

    sample blockchain filles adding

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -2,10 +2,16 @@ const Websocket = require('ws');
 
 const P2P_PORT = process.env.P2P_PORT || 5001;
 const peers =process.env.PEERS ? process.env.PEERS.split(',') : [];
+const MESSAGE_TYPES = {
+    chain:'CHAIN',
+    transaction:'TRANSACTION',
+    clear_transactions:'CLEAR_TRANSACTIONS'
+}
 
 class P2pServer {
-    constructor(blockchain){
+    constructor(blockchain,trasactionPool){
         this.blockchain = blockchain;
+        this.trasactionPool = trasactionPool;
         this.sockets = [];
     }
 
@@ -29,6 +35,55 @@ class P2pServer {
     connectSocket(socket){
         this.sockets.push(socket);
         console.log('Socket connected');
+
+        this.messageHandler(socket);
+
+        socket.send(JSON.stringify(this.blockchain.chain));
+    } 
+    
+    messageHandler(socket){
+        socket.on('message',message =>{
+            const data =JSON.parse(message);
+            switch(data.type){
+                case MESSAGE_TYPES.chain:
+                    this.blockchain.replaceChain(data.chain);
+                    break;
+                case MESSAGE_TYPES.transaction:
+                    this.trasactionPool.updateOrAddTransaction(data.transaction)
+                    break;
+                case MESSAGE_TYPES.clear_transactions:
+                    this.trasactionPool.clear();
+                    break;
+            }
+        });
+    }
+
+    sendChain(socket){
+        socket.send(JSON.stringify({
+            type:MESSAGE_TYPES.chain,
+            chain:this.blockchain.chain
+        }));
+    }
+
+    sendTransaction(socket,transaction){
+        socket.send(JSON.stringify({
+            type:MESSAGE_TYPES.transaction,
+            transaction
+        }));
+    }
+
+    syncChains(){
+        this.sockets.forEach(socket =>this.sendChain(socket));
+    }
+
+    broadcastTransaction(transaction){
+        this.sockets.forEach(socket => this.sendTransaction(socket,transaction));
+    }
+
+    brodcastClearTransaction(){
+        this.sockets.forEach(socket=>socket.send(JSON.stringify({
+            type:MESSAGE_TYPES.clear_transactions
+        })))
     }
 }
 
